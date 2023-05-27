47ceaa664a7db28c192e2c3b2678d48438502813
Koushik Reddy <koushikreddy2005@outlook.com>
2023-05-27 15:17:52 -0700

    Adding files

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import loading from "../assets/loading.gif";
+
+const Spinner = () => {
+  return (
+    <img
+      src={loading}
+      alt="Loading..."
+      style={{
+        width: "100px",
+        margin: "auto",
+        display: "block",
+      }}
+    />
+  );
+};
+
+export default Spinner;
